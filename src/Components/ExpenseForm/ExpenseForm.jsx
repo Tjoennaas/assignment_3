58275f23dfbd8ad64a20db9b579185cb6b1dfa34
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2024-03-24 15:38:43 +0100

    Submison and validation for adding expenses

--- src/Components/ExpenseForm/ExpenseForm.jsx
+++ src/Components/ExpenseForm/ExpenseForm.jsx
@@ -1,74 +1,91 @@
+import  { useState } from 'react';
+import { useExpenseFormValidation, FormFieldError } from '../ValidateForm/ValidateForm'; // Ensure the correct import path
 
+function ExpenseForm({ onAddExpense }) {
+  const [enterTitle, setEnterTitle] = useState("");
+  const [enterExpenseAmount, setEnterExpenseAmount] = useState("");
+  const [enterDate, setEnterDate] = useState("");
+  const [enterCategory, setEnterCategory] = useState("");
 
+  const { errors, validateForm } = useExpenseFormValidation();
 
-import { useSate } from "react" ;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateForm({ 
+       title: enterTitle,
+       expenseAmount: enterExpenseAmount, 
+       date: enterDate,
+       category: enterCategory })) return;
 
+    const newExpense = {
+      id: Math.random().toString(),
+      title: enterTitle,
+      expenseAmount: parseFloat(enterExpenseAmount),
+      date: enterDate,
+      category: enterCategory,
+    };
 
-function ExpenseForm () {
+    onAddExpense(newExpense);
+    setEnterTitle("");
+    setEnterExpenseAmount("");
+    setEnterDate("");
+    setEnterCategory("");
+  };
 
-const [ enterTitle, setEnterTitle ] = useSate ("");
-const [ enterExpenseAmount, setEnterExpenseAmount ] = useSate ("");
-const [ enterDate, setEnterDate ] = useSate ("");
-const [ enterCategory, setEnterCategory ] = useSate ("");
-
-
-return(
-
+  return (
     <form onSubmit={handleSubmit}>
-    <div>
-      <label htmlFor="expenseTitle">Title:</label>
-      <input
-        type="text"
-        id="expenseTitle"
-        name="title"
-        value={enterTitle}
-        onChange={(e) => setEnterTitle(e.target.value)}
-      />
-    </div>
-      
-    <div>
-      <label htmlFor="expenseDate">Date:</label>
-      <input
-        type="date"
-        id="expenseDate"
-        name="date"
-        value={enterDate}
-        onChange={(e) => setEnterDate(e.target.value)}
-      />
-    </div>
-
-    <div>
-      <label htmlFor="expenseAmount">Amount:</label>
-      <input
-        type="number"
-        id="expenseAmount"
-        name="expenseAmount"
-        value={enterExpenseAmount}
-        onChange={(e) => setEnterExpenseAmount(e.target.value)}
-      />
- 
-    </div>
-
-
-    <div>
-      <label htmlFor="expenseCategory">Category:</label>
-      <select
-        id="expenseCategory"
-        name="category"
-        value={enterCategory}
-        onChange={(e) => setEnterCategory(e.target.value)}
-      >
-         <option value="">Choose category</option>
+      <div>
+        <label htmlFor="expenseTitle">Title:</label>
+        <input
+          type="text"
+          id="expenseTitle"
+          name="title"
+          value={enterTitle}
+          onChange={(e) => setEnterTitle(e.target.value)}
+        />
+        <FormFieldError message={errors.title} />
+      </div>
+      <div>
+        <label htmlFor="expenseAmount">Amount:</label>
+        <input
+          type="number"
+          name="expenseAmount"
+          value={enterExpenseAmount}
+          onChange={(e) => setEnterExpenseAmount(e.target.value)}
+        />
+        <FormFieldError message={errors.expenseAmount} />
+      </div>
+      <div>
+        <label htmlFor="expenseDate">Date:</label>
+        <input
+          type="date"
+          name="date"
+          value={enterDate}
+          onChange={(e) => setEnterDate(e.target.value)}
+        />
+        <FormFieldError message={errors.date} />
+      </div>
+      <div>
+        <label htmlFor="expenseCategory">Category:</label>
+        <select
+          type="expenseCategory"
+          name="category"
+          value={enterCategory}
+          onChange={(e) => setEnterCategory(e.target.value)}
+        >
+          <option value="">Choose category</option>
           <option value="housing">Housing</option>
           <option value="grocery">Grocery</option>
           <option value="transportation">Transportation</option>
           <option value="clothes">Clothes</option>
           <option value="other">Other</option>
         </select>
-     </div>
-          <button type="submit">Submit</button>
-     </form>
-   );
-  }
+        <FormFieldError message={errors.category} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
 
     export default ExpenseForm;
