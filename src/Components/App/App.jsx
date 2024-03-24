f03dbe98d6083fc679cc5581baaffc4fa2c401f3
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2024-03-24 14:33:17 +0100

    Adding function to Expenseform and RendedExpenseList

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -7,18 +7,20 @@ function App() {
   const [expenses, setExpenses] = useState([]);        //setExpenses : Use to update the expenses state.
 
   const addExpense = (newExpense) => {
-     setExpenses((prevExpenses) =>         //PrevExpenses: Represents the current state of the expenses before the update.                       
+     setExpenses((prevExpenses) =>            //PrevExpenses: Represents the current state of the expenses before the update.                       
      [...prevExpenses, newExpense]);          //The spread operator ... is used to expand prevExpenses into its individual elements and add new expense to the array.                 
                                                                  
   };
 
-  const removeExpense = (id) => {                            
-    setExpenses((prevExpenses) =>                    // SetExpenses((prevExpenses): Represents the current state of the expenses list before any changes                   
-    prevExpenses.filter((expense) =>                 // PrevExpense.filter:  Iterates over prevExpenses.
-    expense.id !== id));                              //.filter(expense): Decide whether each element should be included in the new array.
-                                                     // expense.id !== id: ID is not the same as the id you want to remove(true)
-                                                     
+
+  const removeExpense = (id) => {  
+
+    setExpenses((prevExpenses) =>             // SetExpenses((prevExpenses): Represents the current state of the expenses list before any changes                   
+    prevExpenses.filter((expense) =>          // PrevExpense.filter:  Iterates over prevExpenses.
+    expense.id !== id));                      //.filter(expense): Decide whether each element should be included in the new array.
+                                              // expense.id !== id: ID is not the same as the id you want to remove(true)                                                 
    };                                   
+
 
   const totalExpenses = expenses.reduce((sum, expense) => 
          sum + expense.expenseAmount, 0);
@@ -26,14 +28,13 @@ function App() {
 
   return (
     <>
+
       <ExpenseForm onAddExpense={addExpense} />
-
-      <RenderdExpenseList expenses={expenses} onRemoveExpense={removeExpense} />
-
+      <RenderdExpenseList expenses={expenses}  onRemoveExpense={removeExpense} />
       <div>Total Expenses: ${totalExpenses.toFixed(2)}</div>
-
+  
     </>
   );
-}
+  }
 
 export default App;
