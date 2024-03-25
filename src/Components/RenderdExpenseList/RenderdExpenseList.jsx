a92731b046763045ede7bbf3d24032c7f3028410
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2024-03-25 15:34:09 +0100

    Styling for better interaction

--- src/Components/RenderdExpenseList/RenderdExpenseList.jsx
+++ src/Components/RenderdExpenseList/RenderdExpenseList.jsx
@@ -1,24 +1,38 @@
 
- 
+
+
+import Styles from "./RenderdExpenseList.module.css";
+
 function RenderdExpenseList({ expenses, onRemoveExpense }) {
     return (
-  
-  
-        <ul>
-          {expenses.map((expense) => (
-            <li key={expense.id}>
-              <span>{expense.title}</span>
-              <span>{expense.category}</span>
-              <span>{expense.date}</span>
-              <span>{expense.expenseAmount.toFixed(2)}</span>     
-             
-              <button onClick={() => onRemoveExpense(expense.id)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-    
+        <>   
+      
+           
+      <h1>Expense Tracker App</h1>
+
+            <header className={Styles.header}>
+                <span>Title:</span>
+                <span>Category:</span>
+                <span>Date:</span>
+                <span>Amount:</span>
+                <span></span>
+            </header>
+
+            <ul className={Styles.renderdList}>
+                {expenses.map((expense) => (
+                    <li key={expense.id} className={Styles.listItem} >
+                        <span>{expense.title}</span>
+                        <span>{expense.category}</span>
+                        <span>{expense.date}</span>
+                        <span>{expense.expenseAmount.toFixed(2)}</span>
+                        <button onClick={() => onRemoveExpense(expense.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+      
+        </>
     );
-   
-  }
+}
+
+export default RenderdExpenseList;
   
-  export default RenderdExpenseList;
