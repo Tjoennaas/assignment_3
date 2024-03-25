b2e91736494c936dea4456175e3dee08ae306387
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2024-03-25 13:57:09 +0100

    Add classNames in App.jsx

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,40 +1,45 @@
 
- import { useState } from 'react';
- import ExpenseForm from '../ExpenseForm/ExpenseForm'; 
- import RenderdExpenseList from '../RenderdExpenseList/RenderdExpenseList'; 
+import { useState } from 'react';
+import ExpenseForm from '../ExpenseForm/ExpenseForm'; 
+import styles from "./App.module.css"
+import RenderdExpenseList from '../RenderdExpenseList/RenderdExpenseList'; 
+
+
 
 function App() {
-  const [expenses, setExpenses] = useState([]);        //setExpenses : Use to update the expenses state.
+ const [expenses, setExpenses] = useState([]);          //setExpenses : Use to update the expenses state.
 
-  const addExpense = (newExpense) => {
-     setExpenses((prevExpenses) =>            //PrevExpenses: Represents the current state of the expenses before the update.                       
-     [...prevExpenses, newExpense]);          //The spread operator ... is used to expand prevExpenses into its individual elements and add new expense to the array.                 
-                                                                 
-  };
-
-
-  const removeExpense = (id) => {  
-
-    setExpenses((prevExpenses) =>             // SetExpenses((prevExpenses): Represents the current state of the expenses list before any changes                   
-    prevExpenses.filter((expense) =>          // PrevExpense.filter:  Iterates over prevExpenses.
-    expense.id !== id));                      //.filter(expense): Decide whether each element should be included in the new array.
-                                              // expense.id !== id: ID is not the same as the id you want to remove(true)                                                 
-   };                                   
+ const addExpense = (newExpense) => {
+    setExpenses((prevExpenses) =>                      //PrevExpenses: Represents the current state of the expenses before the update.                       
+    [...prevExpenses, newExpense]);                    //The spread operator ... is used to expand prevExpenses into its individual elements and add new expense to the array.                 
+                                                                
+ };
 
 
-  const totalExpenses = expenses.reduce((sum, expense) => 
-         sum + expense.expenseAmount, 0);
-         
+ const removeExpense = (id) => {   
+    setExpenses((prevExpenses) =>              // SetExpenses((prevExpenses): Represents the current state of the expenses list before any changes                   
+   prevExpenses.filter((expense) =>            // PrevExpense.filter:  Iterates over prevExpenses.
+   expense.id !== id));                        //.filter(expense): Decide whether each element should be included in the new array.
+                                               // expense.id !== id: ID is not the same as the id you want to remove(true)                                                 
+  };                                   
 
-  return (
-    <>
 
-      <ExpenseForm onAddExpense={addExpense} />
-      <RenderdExpenseList expenses={expenses}  onRemoveExpense={removeExpense} />
-      <div>Total Expenses: ${totalExpenses.toFixed(2)}</div>
-  
-    </>
-  );
-  }
+ const totalExpenses = expenses.reduce((sum, expense) => 
+        sum + expense.expenseAmount, 0);
+        
+
+ return (
+   <>
+     <div className={styles.flexContainer}>
+     <ExpenseForm onAddExpense={addExpense} />
+     <div className={styles.renderdList}>
+     <RenderdExpenseList expenses={expenses} onRemoveExpense={removeExpense} />
+     
+     <div className={styles.totalExpenses}> Total Expenses: $ {totalExpenses.toFixed(2)}</div>
+   </div>
+   </div>
+   </>
+ );
+ }
 
 export default App;
